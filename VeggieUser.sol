eddc8af8ab8e9e3432f3d789bcfc1a24fb88bcb8
ChienWenTweety <42764696+ChienWenTweety@users.noreply.github.com>
2019-07-27 15:30:52 +0800

    Update VeggieUser.sol

--- VeggieUser.sol
+++ VeggieUser.sol
@@ -1,54 +1,55 @@
 pragma solidity ^0.4.23;
+
 import "./VeggieCertificationStorage.sol";
 import "./Ownable.sol";
 
-contract VeggieUser is Ownable
-{
-     /*Events*/ 
-    event UserUpdate(address indexed user, string name, string contactNo, string role, bool isActive, string profileHash);
+contract VeggieUser is Ownable {
+    
+    /* Events */ 
+    event UserUpdate(address indexed user, string name, string contactNo, string role, bool isActive);
     event UserRoleUpdate(address indexed user, string role); 
     
-     /* Storage Variables */    
+    /* Storage Variables */    
     VeggieCertificationStorage veggieCertificationStorage;
     
-     constructor(address _veggieAddress) public {
+    constructor(address _veggieAddress) public {
         veggieCertificationStorage = VeggieCertificationStorage(_veggieAddress);
     }   
     
     
-     /* Create/Update User */
-
-    function updateUser(string _name, string _contactNo, string _role, bool _isActive,string _profileHash) public returns(bool)
-    {
+    /* Create/Update User */
+    function updateUser(string _name, string _contactNo, string _role, bool _isActive) public returns(bool) {
+        
         require(msg.sender != address(0));
         
         /* Call Storage Contract */
-        bool status = veggieCertificationStorage.setUser(msg.sender, _name, _contactNo, _role, _isActive,_profileHash);
-        
-         /*call event*/
-        emit UserUpdate(msg.sender,_name,_contactNo,_role,_isActive,_profileHash);
-        emit UserRoleUpdate(msg.sender,_role);
+        bool status = veggieCertificationStorage.setUser(msg.sender, _name, _contactNo, _role, _isActive);
+    
+        /*call event*/
+        emit UserUpdate(msg.sender, _name, _contactNo, _role, _isActive);
+        emit UserRoleUpdate(msg.sender, _role);
         
         return status;
     }
     
     /* Create/Update User For Admin  */
-    function updateUserForAdmin(address _userAddress, string _name, string _contactNo, string _role, bool _isActive,string _profileHash) public onlyOwner returns(bool)
-    {
+    function updateUserForAdmin(address _userAddress, string _name, string _contactNo, string _role, bool _isActive) public onlyOwner returns(bool) {
+        
         require(_userAddress != address(0));
         
         /* Call Storage Contract */
-        bool status = veggieCertificationStorage.setUser(_userAddress, _name, _contactNo, _role, _isActive, _profileHash);
+        bool status = veggieCertificationStorage.setUser(_userAddress, _name, _contactNo, _role, _isActive);
         
-         /*call event*/
-        emit UserUpdate(_userAddress,_name,_contactNo,_role,_isActive,_profileHash);
-        emit UserRoleUpdate(_userAddress,_role);
+        /*call event*/
+        emit UserUpdate(_userAddress, _name, _contactNo, _role, _isActive);
+        emit UserRoleUpdate(_userAddress, _role);
         
         return status;
     }
     
     /* get User */
-    function getUser(address _userAddress) public view returns(string name, string contactNo, string role, bool isActive , string profileHash){
+    function getUser(address _userAddress) public view returns(string name, string contactNo, string role, bool isActive, string profileHash) {
+        
         require(_userAddress != address(0));
         
         /*Getting value from struct*/
